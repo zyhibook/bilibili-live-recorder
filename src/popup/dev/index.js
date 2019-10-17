1fa50ed9bbc6eb486c2d3c85123fec99d5aec3a9
Harvey <laozhaochaguan@gmail.com>
2019-10-17 17:33:17 +0800

    设置页面

--- src/popup/dev/index.js
+++ src/popup/dev/index.js
@@ -1,8 +1,80 @@
+import 'normalize.css';
 import './index.scss';
 
 class Popup {
     constructor() {
-        console.log(this.constructor.name);
+        this.$headerItems = Array.from(document.querySelectorAll('.header .item'));
+        this.$mainPanels = Array.from(document.querySelectorAll('.main .panel'));
+        this.$fileName = document.querySelector('.fileName');
+        this.$disabled = document.querySelector('.disabled');
+        this.$limit = document.querySelector('.limit');
+        this.$limitSize = document.querySelector('.limit_size');
+        this.$limitDuration = document.querySelector('.limit_duration');
+        this.$size = document.querySelector('.size');
+        this.$duration = document.querySelector('.duration');
+        this.$fileSize = document.querySelector('.fileSize');
+        this.$fileDuration = document.querySelector('.fileDuration');
+
+        this.initInfo();
+        this.initSwitch();
+        this.initLimit();
+    }
+
+    initInfo() {
+        chrome.tabs.query(
+            {
+                active: true,
+            },
+            tabs => {
+                if (tabs && tabs[0]) {
+                    const tab = tabs[0];
+                    if (tab.url.startsWith('https://live.bilibili.com')) {
+                        this.$fileName.value = tab.title;
+                    } else {
+                        // this.$disabled.classList.add('show');
+                    }
+                }
+            },
+        );
+    }
+
+    initSwitch() {
+        this.$headerItems.forEach(($item, $index) => {
+            $item.addEventListener('click', () => {
+                this.$headerItems.forEach(item => {
+                    if ($item === item) {
+                        item.classList.add('active');
+                    } else {
+                        item.classList.remove('active');
+                    }
+                });
+                this.$mainPanels.forEach((item, index) => {
+                    if ($index === index) {
+                        item.classList.add('active');
+                    } else {
+                        item.classList.remove('active');
+                    }
+                });
+            });
+        });
+    }
+
+    initLimit() {
+        this.$fileSize.addEventListener('change', () => {
+            this.$size.textContent = this.$fileSize.value + 'M';
+        });
+        this.$fileDuration.addEventListener('change', () => {
+            this.$duration.textContent = this.$fileDuration.value + '分钟';
+        });
+        this.$limit.addEventListener('change', () => {
+            [this.$limitSize, this.$limitDuration].forEach($item => {
+                if ($item.classList.contains(this.$limit.value)) {
+                    $item.classList.add('active');
+                } else {
+                    $item.classList.remove('active');
+                }
+            });
+        });
     }
 }
 
--- src/popup/index.js
+++ src/popup/index.js
@@ -12,11 +12,113 @@
 
   var classCallCheck = _classCallCheck;
 
-  var Popup = function Popup() {
-    classCallCheck(this, Popup);
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
 
-    console.log(this.constructor.name);
-  };
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
+
+  var createClass = _createClass;
+
+  var Popup =
+  /*#__PURE__*/
+  function () {
+    function Popup() {
+      classCallCheck(this, Popup);
+
+      this.$headerItems = Array.from(document.querySelectorAll('.header .item'));
+      this.$mainPanels = Array.from(document.querySelectorAll('.main .panel'));
+      this.$fileName = document.querySelector('.fileName');
+      this.$disabled = document.querySelector('.disabled');
+      this.$limit = document.querySelector('.limit');
+      this.$limitSize = document.querySelector('.limit_size');
+      this.$limitDuration = document.querySelector('.limit_duration');
+      this.$size = document.querySelector('.size');
+      this.$duration = document.querySelector('.duration');
+      this.$fileSize = document.querySelector('.fileSize');
+      this.$fileDuration = document.querySelector('.fileDuration');
+      this.initInfo();
+      this.initSwitch();
+      this.initLimit();
+    }
+
+    createClass(Popup, [{
+      key: "initInfo",
+      value: function initInfo() {
+        var _this = this;
+
+        chrome.tabs.query({
+          active: true
+        }, function (tabs) {
+          if (tabs && tabs[0]) {
+            var tab = tabs[0];
+
+            if (tab.url.startsWith('https://live.bilibili.com')) {
+              _this.$fileName.value = tab.title;
+            }
+          }
+        });
+      }
+    }, {
+      key: "initSwitch",
+      value: function initSwitch() {
+        var _this2 = this;
+
+        this.$headerItems.forEach(function ($item, $index) {
+          $item.addEventListener('click', function () {
+            _this2.$headerItems.forEach(function (item) {
+              if ($item === item) {
+                item.classList.add('active');
+              } else {
+                item.classList.remove('active');
+              }
+            });
+
+            _this2.$mainPanels.forEach(function (item, index) {
+              if ($index === index) {
+                item.classList.add('active');
+              } else {
+                item.classList.remove('active');
+              }
+            });
+          });
+        });
+      }
+    }, {
+      key: "initLimit",
+      value: function initLimit() {
+        var _this3 = this;
+
+        this.$fileSize.addEventListener('change', function () {
+          _this3.$size.textContent = _this3.$fileSize.value + 'M';
+        });
+        this.$fileDuration.addEventListener('change', function () {
+          _this3.$duration.textContent = _this3.$fileDuration.value + '分钟';
+        });
+        this.$limit.addEventListener('change', function () {
+          [_this3.$limitSize, _this3.$limitDuration].forEach(function ($item) {
+            if ($item.classList.contains(_this3.$limit.value)) {
+              $item.classList.add('active');
+            } else {
+              $item.classList.remove('active');
+            }
+          });
+        });
+      }
+    }]);
+
+    return Popup;
+  }();
 
   var index = new Popup();
 
